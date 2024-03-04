0b1fba71d7534d3ec8fa7511e9c94b221a5cc9cd
openmpy <suhwan@kakao.com>
2024-03-04 18:04:13 +0900

    feat: 회원가입 기능

--- src/main/java/com/example/mission04/domain/member/dto/MemberRequestDto.java
+++ src/main/java/com/example/mission04/domain/member/dto/MemberRequestDto.java
@@ -1,4 +1,46 @@
 package com.example.mission04.domain.member.dto;
 
+import com.example.mission04.domain.member.entity.Member;
+import com.example.mission04.domain.member.entity.type.AuthorityType;
+import com.example.mission04.domain.member.entity.type.GenderType;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+
 public class MemberRequestDto {
+
+    @Getter
+    public static class SignupRequestDto {
+
+        @Email(message = "이메일 형식이 아닙니다.")
+        private String email;
+
+        @Pattern(
+                message = "최소 8자 이상, 15자 이하이며 알파벳 대소문자(a~z, A~Z), 숫자(0~9), 특수문자로 구성되어야 합니다.",
+                regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,15}$"
+        )
+        private String password;
+
+        private GenderType gender;
+
+        @NotBlank(message = "전화번호를 입력해주세요.")
+        private String phone;
+
+        @NotBlank(message = "주소를 입력해주세요.")
+        private String address;
+
+        private AuthorityType authority;
+
+        public Member toEntity(String encodedPassword) {
+            return Member.builder()
+                    .email(this.email)
+                    .password(encodedPassword)
+                    .phone(this.phone)
+                    .gender(this.gender)
+                    .address(this.address)
+                    .authority(this.authority)
+                    .build();
+        }
+    }
 }
--- src/main/java/com/example/mission04/domain/member/dto/MemberResponseDto.java
+++ src/main/java/com/example/mission04/domain/member/dto/MemberResponseDto.java
@@ -1,4 +1,29 @@
 package com.example.mission04.domain.member.dto;
 
+import com.example.mission04.domain.member.entity.Member;
+import com.example.mission04.domain.member.entity.type.AuthorityType;
+import com.example.mission04.domain.member.entity.type.GenderType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 public class MemberResponseDto {
+
+    @AllArgsConstructor
+    @Getter
+    public static class SignupResponseDto {
+
+        private String email;
+        private String phone;
+        private GenderType gender;
+        private String address;
+        private AuthorityType authority;
+
+        public SignupResponseDto(Member member) {
+            this.email = member.getEmail();
+            this.phone = member.getPhone();
+            this.gender = member.getGender();
+            this.address = member.getAddress();
+            this.authority = member.getAuthority();
+        }
+    }
 }
