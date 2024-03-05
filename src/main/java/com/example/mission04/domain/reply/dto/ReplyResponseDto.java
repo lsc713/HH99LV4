5124f3922b9b50216ab7cf9f883fdac58fecd13a
김수환 <suhwan@kakao.com>
2024-03-05 10:42:12 +0900

    feat: 대댓글 기능 추가 (#20)
    - 대댓글 작성
    - 대댓글 조회
    - 대댓글 수정
    - 대댓글 삭제

--- src/main/java/com/example/mission04/domain/reply/dto/ReplyResponseDto.java
+++ src/main/java/com/example/mission04/domain/reply/dto/ReplyResponseDto.java
@@ -1,4 +1,45 @@
 package com.example.mission04.domain.reply.dto;
 
+import com.example.mission04.domain.reply.entity.Reply;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
 public class ReplyResponseDto {
+
+    @Getter
+    public static class CreateReplyResponseDto {
+
+        private final String contents;
+        private final LocalDateTime createdAt;
+
+        public CreateReplyResponseDto(Reply reply) {
+            this.contents = reply.getContents();
+            this.createdAt = reply.getCreatedAt();
+        }
+    }
+
+    @Getter
+    public static class EditReplyResponseDto {
+
+        private final String contents;
+
+        public EditReplyResponseDto(Reply reply) {
+            this.contents = reply.getContents();
+        }
+    }
+
+    @Getter
+    public static class GetReplyResponseDto {
+
+        private final String contents;
+        private final String writer;
+        private final LocalDateTime createdAt;
+
+        public GetReplyResponseDto(Reply reply) {
+            this.contents = reply.getContents();
+            this.writer = reply.getMember().getEmail();
+            this.createdAt = reply.getCreatedAt();
+        }
+    }
 }
--- src/main/java/com/example/mission04/domain/reply/service/ReplyService.java
+++ src/main/java/com/example/mission04/domain/reply/service/ReplyService.java
@@ -1,7 +1,88 @@
 package com.example.mission04.domain.reply.service;
 
+import com.example.mission04.domain.comment.entity.Comment;
+import com.example.mission04.domain.comment.repository.CommentRepository;
+import com.example.mission04.domain.member.entity.Member;
+import com.example.mission04.domain.member.repository.MemberRepository;
+import com.example.mission04.domain.reply.dto.ReplyRequestDto.CreateReplyRequestDto;
+import com.example.mission04.domain.reply.dto.ReplyRequestDto.EditReplyRequestDto;
+import com.example.mission04.domain.reply.dto.ReplyResponseDto.CreateReplyResponseDto;
+import com.example.mission04.domain.reply.dto.ReplyResponseDto.EditReplyResponseDto;
+import com.example.mission04.domain.reply.dto.ReplyResponseDto.GetReplyResponseDto;
+import com.example.mission04.domain.reply.entity.Reply;
+import com.example.mission04.domain.reply.repository.ReplyRepository;
+import com.example.mission04.global.handler.exception.CustomApiException;
+import com.example.mission04.global.handler.exception.ErrorCode;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class ReplyService {
+
+    private final ReplyRepository replyRepository;
+    private final CommentRepository commentRepository;
+    private final MemberRepository memberRepository;
+
+    public ReplyService(ReplyRepository replyRepository, CommentRepository commentRepository, MemberRepository memberRepository) {
+        this.replyRepository = replyRepository;
+        this.commentRepository = commentRepository;
+        this.memberRepository = memberRepository;
+    }
+
+    @Transactional
+    public CreateReplyResponseDto create(Long commentId, String email, CreateReplyRequestDto requestDto) {
+        Member member = memberRepository.findByEmail(email).orElseThrow(() ->
+                new CustomApiException(ErrorCode.MEMBER_ACCOUNT_NOT_FOUND.getMessage())
+        );
+        Comment comment = commentRepository.findById(commentId).orElseThrow(() ->
+                new CustomApiException(ErrorCode.COMMENT_ID_NOT_FOUND.getMessage())
+        );
+
+        Reply reply = replyRepository.save(requestDto.toEntity(member, comment));
+        return new CreateReplyResponseDto(reply);
+    }
+
+    @Transactional
+    public EditReplyResponseDto edit(Long commentId, Long replyId, String email, EditReplyRequestDto requestDto) {
+        if (!memberRepository.existsByEmail(email)) {
+            throw new CustomApiException(ErrorCode.MEMBER_ACCOUNT_NOT_FOUND.getMessage());
+        }
+        if (!commentRepository.existsById(commentId)) {
+            throw new CustomApiException(ErrorCode.COMMENT_ID_NOT_FOUND.getMessage());
+        }
+        Reply reply = replyRepository.findById(replyId).orElseThrow(() ->
+                new CustomApiException(ErrorCode.REPLY_ID_NOT_FOUND.getMessage())
+        );
+
+        reply.update(requestDto.getContents());
+        return new EditReplyResponseDto(reply);
+    }
+
+    @Transactional
+    public void delete(Long commentId, Long replyId, String email) {
+        if (!memberRepository.existsByEmail(email)) {
+            throw new CustomApiException(ErrorCode.MEMBER_ACCOUNT_NOT_FOUND.getMessage());
+        }
+        if (!commentRepository.existsById(commentId)) {
+            throw new CustomApiException(ErrorCode.COMMENT_ID_NOT_FOUND.getMessage());
+        }
+        Reply reply = replyRepository.findById(replyId).orElseThrow(() ->
+                new CustomApiException(ErrorCode.REPLY_ID_NOT_FOUND.getMessage())
+        );
+
+        replyRepository.delete(reply);
+    }
+
+    @Transactional(readOnly = true)
+    public List<GetReplyResponseDto> getRepliesOfComment(Long commentId) {
+        Comment comment = commentRepository.findById(commentId).orElseThrow(() ->
+                new CustomApiException(ErrorCode.COMMENT_ID_NOT_FOUND.getMessage())
+        );
+
+        return replyRepository.findAllByCommentOrderByCreatedAtDesc(comment).stream()
+                .map(GetReplyResponseDto::new)
+                .toList();
+    }
 }
