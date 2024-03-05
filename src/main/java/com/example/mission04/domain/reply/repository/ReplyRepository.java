73694b6d35ae96fb149043c859597f0643ae25fe
openmpy <suhwan@kakao.com>
2024-03-05 10:38:19 +0900

    feat: 대댓글 기능 추가
    - 대댓글 작성
    - 대댓글 조회
    - 대댓글 수정
    - 대댓글 삭제

--- src/main/java/com/example/mission04/domain/reply/repository/ReplyRepository.java
+++ src/main/java/com/example/mission04/domain/reply/repository/ReplyRepository.java
@@ -1,7 +1,12 @@
 package com.example.mission04.domain.reply.repository;
 
+import com.example.mission04.domain.comment.entity.Comment;
 import com.example.mission04.domain.reply.entity.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
+
+    List<Reply> findAllByCommentOrderByCreatedAtDesc(Comment comment);
 }
--- src/main/java/com/example/mission04/global/handler/exception/ErrorCode.java
+++ src/main/java/com/example/mission04/global/handler/exception/ErrorCode.java
@@ -7,6 +7,8 @@ public enum ErrorCode {
     EMAIL_ALREADY_EXISTS("이미 가입된 이메일입니다."),
     PHONE_ALREADY_EXISTS("이미 가입된 전화번호입니다."),
     MEMBER_ACCOUNT_NOT_FOUND("찾을 수 없는 계정입니다."),
+    COMMENT_ID_NOT_FOUND("찾을 수 없는 댓글 번호입니다."),
+    REPLY_ID_NOT_FOUND("찾을 수 없는 대댓글 번호입니다."),
     ;
 
     private final String message;
