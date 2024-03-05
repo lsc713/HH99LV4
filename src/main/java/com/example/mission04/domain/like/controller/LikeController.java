b5f4e81288af5c7f2625ef1143d48b4ebf82227e
김수환 <suhwan@kakao.com>
2024-03-05 20:01:17 +0900

    feat: swagger 적용 (#27)

--- src/main/java/com/example/mission04/domain/like/controller/LikeController.java
+++ src/main/java/com/example/mission04/domain/like/controller/LikeController.java
@@ -3,6 +3,8 @@ package com.example.mission04.domain.like.controller;
 import com.example.mission04.domain.like.service.LikeService;
 import com.example.mission04.global.dto.ResponseDto;
 import com.example.mission04.global.security.UserDetailsImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicReference;
 
+@Tag(name = "likes", description = "좋아요 관련 API")
 @RequestMapping("/api/v1/lectures")
 @RestController
 public class LikeController {
@@ -22,6 +25,7 @@ public class LikeController {
     }
 
     @PostMapping("/{lectureId}/likes")
+    @Operation(summary = "선택한 강의 좋아요 기능", description = "선택한 강의에 좋아요를 누를 수 있는 API")
     public ResponseDto<AtomicReference<String>> likes(
             @PathVariable Long lectureId,
             @AuthenticationPrincipal UserDetailsImpl userDetails
--- src/main/java/com/example/mission04/domain/teacher/controller/TeacherController.java
+++ src/main/java/com/example/mission04/domain/teacher/controller/TeacherController.java
@@ -6,12 +6,15 @@ import com.example.mission04.domain.teacher.dto.TeacherResponseDto.CreateTeacher
 import com.example.mission04.domain.teacher.service.TeacherService;
 import com.example.mission04.global.dto.ResponseDto;
 import com.example.mission04.global.security.UserDetailsImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "teachers", description = "강사 관련 API")
 @RequestMapping("/api/v1/teachers")
 @RestController
 public class TeacherController {
@@ -25,6 +28,7 @@ public class TeacherController {
     @PostMapping
     @Secured(Authority.ADMIN)
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "강사 등록 기능", description = "강사를 등록할 수 있는 API")
     public ResponseDto<CreateTeacherResponseDto> create(
             @AuthenticationPrincipal UserDetailsImpl userDetails,
             @RequestBody @Valid CreateTeacherRequestDto requestDto
